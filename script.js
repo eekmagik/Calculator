04e058897ae4a6ac46c89b7e17f1f5f2b47f2fb4
eekmagik <dx3906@protonmail.com>
2022-06-27 14:41:40 -0700

    Create operator functions

---
+++ script.js
@@ -0,0 +1,29 @@
+
+//operator functions
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a,b) {
+  return a*b;
+};
+
+const divide = function(a,b) {
+  return (a/b);
+};
+
+const operators = {
+  add,
+  subtract,
+  multiply,
+  divide,
+}
+
+
+const operate = function(operator, a,b) {
+  operators.operator(a,b);
+}
