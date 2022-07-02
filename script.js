31b3425f5d87f293670bb4ae9d6bfb457f260660
eekmagik <dx3906@protonmail.com>
2022-07-02 14:13:37 -0700

    Determine character limit at min width

--- script.js
+++ script.js
@@ -16,14 +16,15 @@ const divide = function(a,b) {
   return (a/b);
 };
 
-const operators = {
-  add,
-  subtract,
-  multiply,
-  divide,
-}
+const operate = function(operator,a,b) {
+  alert(operator(a,b));
+};
 
+const input_current = document.querySelector('.input-current');
 
-const operate = function(operator, a,b) {
-  operators.operator(a,b);
-}
+const display_value = document.createElement('div');
+
+//display_value has a 19 char limit at min width
+display_value.textContent = "3.14159265358979323";
+
+input_current.appendChild(display_value);
