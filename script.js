3c0f875e7271214c6fa24cc070c93d8f01fc2cd4
eek magik <dx3906@protonmail.com>
2022-10-06 21:50:08 -0700

    Handle results

--- script.js
+++ script.js
@@ -1,70 +1,88 @@
 
 //operator functions
 const add = function(a, b) {
-	return a + b;
+	return (a+b);
 };
 
 const subtract = function(a, b) {
-	return a - b;
+	return (a - b);
+  
 };
 
 const multiply = function(a,b) {
-  return a*b;
+  return (a*b);
+ 
 };
 
 const divide = function(a,b) {
   return (a/b);
+  
 };
 
-const operate = function(operation,a,b) {
-  return (operation(a,b));
+const operate = function(a,b) {
+  result = operator(a,b);
+  toDisplay(result)
+  histOperand1 = operand1;
+  histoperand2 = operand2;
+  operand1 = result;
+  return result;
 };
 
 
 const inputCurrent = document.querySelector('.input-current');
 const displayValue = document.createElement('div');
-let operator = "";
-let operand1 = "";
-let operand2 = "";
+
+let operator = NaN;
+let operand = NaN;
+let operand1 = NaN;
+let operand2 = NaN;
+let result = NaN;
+
+const numbers = function(number){
+  
+  if (isNaN(operand)) {
+    toDisplay(number);
+    return (operand = number);
+  };
+
+
+  if (operand.length === 18) {
+    return;
+  }
+  
+  else {
+    operand = parseInt("" + operand + number);
+    toDisplay(operand);
+    return operand;
+  };
+};
+
+const setOperand = function(number) {
+  if (isNaN(operand1) === false){
+    operand2 = number;
+  };
+  operand1 = number;
+  operand = NaN;
+};
 
 const setOperator = function(variable) {
-  if (operand1 === "") {
-    return;
+  if (typeof operator != 'function') {
+    operator = variable;
+    setOperand(operand);
+    operand = NaN;
+    toDisplay('');
   };
-  if (operator === "") {
-    return operator = variable;
+  if ((typeof operator === 'function') && (isNaN(operand1) === false)) {
+    operator = variable;
   };
-  if ((operator.length > 0) && operand1 != "") {
-    return operator = variable;
+  if ((typeof operator === 'function') && (isNaN(operand) === false)) {
+    result = operate();
   };
-  if ((operator.length > 0) && operand2 != "") {
-    toDisplay(operate(variable,operand1, operand2));
 };
 
 const toDisplay = function(number) {
-  if (operand1 === "" && number != ".") {
-    displayValue.textContent = number;
-    inputCurrent.appendChild(displayValue);
-    return operand1 = displayValue.textContent; 
-  }
-  if (operand1.length === 18) {
-    return;
-  }
-
-  if (number === "." && operand1.includes(number)) {
-    return;
-  }
-
-  if (operand1 === ".") {
-    displayValue.textContent = "0." + number;
-    inputCurrent.appendChild(displayValue);
-  return operand1 = displayValue.textContent; 
-  }
-  
-  displayValue.textContent = operand1 + number;
-  inputCurrent.appendChild(displayValue);
-  return operand1 = displayValue.textContent; 
-  
+    displayValue.textContent = number
+    inputCurrent.appendChild(displayValue)
 };
 
 //button inputs
@@ -72,92 +90,96 @@ const toDisplay = function(number) {
 const zeroButton = document.querySelector('#zero');
 zeroButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('0');
+  numbers(0);
 });
 const oneButton = document.querySelector('#one');
 oneButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('1');
+  numbers(1);
 });
 const twoButton = document.querySelector('#two');
 twoButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('2');
+  numbers(2);
 });
 const threeButton = document.querySelector('#three');
 threeButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('3');
+  numbers(3);
 });
 const fourButton = document.querySelector('#four');
 fourButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('4');
+  numbers(4);
 });
 const fiveButton = document.querySelector('#five');
 fiveButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('5');
+  numbers(5);
 });
 const sixButton = document.querySelector('#six');
 sixButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('6');
+  numbers(6);
 });
 const sevenButton = document.querySelector('#seven');
 sevenButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('7');
+  numbers(7);
 });
 const eightButton = document.querySelector('#eight');
 eightButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('8');
+  numbers(8);
 });
 const nineButton = document.querySelector('#nine');
 nineButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('9');
+  numbers(9);
 });
 const decimalButton = document.querySelector('#decimal');
 decimalButton.addEventListener("click", () => {
   // clickIndicator();
-  toDisplay('.');
+  
 });
 
 const equalsButton = document.querySelector('#equals');
 equalsButton.addEventListener("click", () => {
   // clickIndicator();
-  
+  operand2 = operand;
+  operate(operand1,operand2);
 });
+
 const addButton = document.querySelector('#add');
 addButton.addEventListener("click", () => {
   // clickIndicator();
-  setOperator('add');
+  setOperator(add);
 });
 
 const subtractButton = document.querySelector('#subtract');
 subtractButton.addEventListener("click", () => {
   // clickIndicator();
-  setOperator('subtract');
+  setOperator(subtract);
 });
 
 const multiplyButton = document.querySelector('#multiply');
 multiplyButton.addEventListener("click", () => {
   // clickIndicator();
-  setOperator('multiply');
+  setOperator(multiply);
 });
 
 const divideButton = document.querySelector('#divide');
 divideButton.addEventListener("click", () => {
   // clickIndicator();
-  setOperator('divide');
+  setOperator(divide);
 });
 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener("click", () => {
   // clickIndicator();
-  operand1 = "";
+  operand = NaN;
+  operator = NaN;
+  result = NaN;
   toDisplay('');
 });
 //const button = document.querySelector(${})
@@ -167,5 +189,3 @@ clearButton.addEventListener("click", () => {
 
 
 //display_value has a 19 char limit at min width
-
-
